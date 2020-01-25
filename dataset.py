ea21a21580b92491c7fad1a7ccab8ffe697889a6
Jai <jaihebel@gmail.com>
2020-01-24 16:59:53 -0800

    Refactored and added vae training module

---
+++ dataset.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+
+import logging
+import torchvision as tv
+import torch
+from pathlib import Path
+
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class HCSData(torch.utils.data.Dataset):
+    """
+    High Content Screening Dataset (BBBC022)
+    """
+
+    def __init__(self, data):
+        """
+        Args:
+            data (DataFrame) : pandas dataframe of metadata
+        """
+        self.df = data
+        self.root = Path("./data/raw/")
+
+    @classmethod
+    def from_csv(cls, csv_file):
+        """
+        Constructor to generate a dataset from a csv file
+        """
+        return cls(pd.read_csv(csv_file, index_col=0))
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        """
+        Args:
+            idx (int) : Index of item to grab
+        """
+        sample = self.df.iloc[idx]  # Grab sample at index idx
+
+        x = torch.from_numpy(self.__load_img__(sample)).type(
+            torch.float)  # Load images
+        y = sample['ROLE']  # Load label
+
+        # Encode y labels to binary
+        label_enc = {  # Dictionary of label encoding
+            'mock': 0,
+            'compound': 1
+        }
+        # Get enc from label, default = 'mock'
+        y = label_enc.get(y, 0)
+
+        return x, y
+
+    def __one_hot_enc(self, label, n_labels):
+        a = torch.zeros(n_labels, dtype=torch.int64)
+        a[label] = 1
+        return a.type(torch.float)
+
+    def __load_img__(self, sample):
+        """
+        Load each image channel for the given sample and stack them into a
+        single 5-channel 2D image
+        """
+        plate = sample['PLATE']  # Get plate num.
+
+        # Load each tiff file individually
+        hoechst_path = self.root / \
+            f"BBBC022_v1_images_{plate}w1/{sample['FileHoechst']}"
+        hoechst = plt.imread(hoechst_path)
+        er_path = self.root / \
+            f"BBBC022_v1_images_{plate}w2/{sample['FileER']}"
+        er = plt.imread(er_path)
+        syto_path = self.root / \
+            f"BBBC022_v1_images_{plate}w3/{sample['FileSyto']}"
+        syto = plt.imread(syto_path)
+        ph_path = self.root / \
+            f"BBBC022_v1_images_{plate}w4/{sample['FilePh']}"
+        ph = plt.imread(ph_path)
+        mito_path = self.root / \
+            f"BBBC022_v1_images_{plate}w5/{sample['FileMito']}"
+        mito = plt.imread(mito_path)
+
+        # Stack images in channel (x, y, c) dimension
+        img = np.stack([hoechst, er, syto, ph, mito])
+
+        return img.astype(np.float64())
+
+    def split(self, ratio):
+        """
+        Split the dataset into two subsets defined by the ration
+        ration (double) : dataset is split into sizes of [ration, 1-ratio]
+        """
+        try:
+            assert (ratio < 1) & (ratio > 0), \
+                "Train-test split should be greater than 0 and less than 1."
+
+            # Stratify by class
+            mock = self.df[self.df['ROLE'] == 'mock']
+            compound = self.df[self.df['ROLE'] == 'compound']
+
+            mock_idx = np.round(ratio * len(mock)).astype(np.int)
+            comp_idx = np.round(ratio * len(compound)).astype(np.int)
+
+            train = pd.concat([mock.iloc[:mock_idx], compound.iloc[:comp_idx]])
+            test = pd.concat([mock.iloc[mock_idx:], compound.iloc[comp_idx:]])
+
+            return self.__class__(train), self.__class__(test)
+
+        except AssertionError as error:
+            logging.exception(error)
--- train_vae.py
+++ train_vae.py
@@ -1,18 +1,12 @@
-import pandas as pd
-import numpy as np
-from tqdm import tqdm
 import click
 import yaml
-
-# import cli_args
-
+from tqdm import tqdm
 import logging
-import torchvision as tv
+
 import torch
-from pathlib import Path
 
 from dataset import HCSData
-from models import VGG16, VAE
+from models import VAE
 
 
 logging.basicConfig(level=logging.INFO)
@@ -22,13 +16,21 @@ with open("./configs/params.yml", 'r') as f:
     p = yaml.load(f)
 
 
+def vae_loss(output, input, mean, logvar, loss_func):
+    recon_loss = loss_func(output, input)
+    kl_loss = torch.mean(0.5 * torch.sum(
+        torch.exp(logvar) + mean**2 - 1. - logvar, 1))
+    return recon_loss + kl_loss
+
+
 @click.command()
 @click.option("--csv_file", type=click.Path(exists=True), default=p['csv_file'])
 @click.option("--debug/--no-debug", default=p['debug'])
 @click.option("-e", "--epochs", type=int, default=p['epochs'])
 @click.option("-b", "--batch_size", type=int, default=p['batch_size'])
 @click.option("-s", "--split", type=float, default=p['split'])
-def train(csv_file, debug, epochs, batch_size, split):
+@click.option("-z", "--n_features", type=int, default=32)
+def train(csv_file, debug, epochs, batch_size, split, n_features):
     # Set up gpu/cpu device
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -41,37 +43,37 @@ def train(csv_file, debug, epochs, batch_size, split):
     test_loader = torch.utils.data.DataLoader(  # Generate a testing data loader
         test, batch_size=batch_size, shuffle=False)
 
-    # Define Model
-    net = VGG16()
+    in_shape = tuple(data[0][0].shape)
+    net = VAE(in_shape, n_features)
+
     # Move Model to GPU
     if torch.cuda.device_count() > 1:  # If multiple gpu's
         net = torch.nn.DataParallel(net)  # Parallelize
     net.to(device)  # Move model to device
 
     # Define loss and optimizer
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(net.parameters())
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
 
     print("Training...")
 
-    tr_correct = 0
-    tr_total = 0
-    correct = 0
-    total = 0
     # Training
     for epoch in range(epochs):  # Iter through epochcs
         cum_loss = 0
         msg = f"Training epoch {epoch+1}: "
         ttl = len(train_loader)  # Iter through batches
-        for batch_n, (X, Y) in enumerate(train_loader):
-            x, y = X.to(device), Y.to(device)  # Move batch samples to gpu
+        for batch_n, (X, _) in tqdm(enumerate(train_loader), msg, ttl):
+            x = X.to(device)  # Move batch samples to gpu
 
-            o = net(x)  # Forward pass
+            o, u, logvar = net(x)  # Forward pass
             optimizer.zero_grad()  # Reset gradients
-            loss = criterion(o, y)  # Compute Loss
+            # loss = criterion(o, x)
+            loss = vae_loss(o, x, u, logvar, criterion)  # Compute Loss
             loss.backward()  # Propagate loss, compute gradients
             optimizer.step()  # Update weights
+
+            print(loss.item())
 
             cum_loss += loss.item()
 
@@ -82,40 +84,9 @@ def train(csv_file, debug, epochs, batch_size, split):
             #     f" \t Label: {y.item()}"
             # ))
 
-            # PERFORM SOME VALIDATION METRIC
-            _, predicted = torch.max(o.data, 1)
-            tr_total += y.size(0)
-            tr_correct += (predicted == y).sum().item()
-
         # scheduler.step(metric)  # Update the learning rate
 
         print(f"Training loss: {cum_loss:.2f}")
-
-        with torch.no_grad():
-
-            msg = f"Testing epoch {epoch+1}: "
-            ttl = len(test_loader)  # Iter through batches
-            for batch_n, (X, Y) in enumerate(test_loader):
-                x, y = X.to(device), Y.to(device)  # Move batch samples to gpu
-                o = net(x)  # Forward pass
-
-                # PERFORM SOME VALIDATION METRIC
-                _, predicted = torch.max(o.data, 1)
-                total += y.size(0)
-                correct += (predicted == y).sum().item()
-
-            print(f"Epoch {epoch}:")
-            if epoch % 10 == 0:
-                print('Accuracy of the network on the train images: %d %%' % (
-                    100 * tr_correct / tr_total))
-                print('Accuracy of the network on the test images: %d %%' % (
-                    100 * correct / total))
-                correct = 0
-                total = 0
-                tr_correct = 0
-                tr_total = 0
-
-        scheduler.step(correct / total)
 
 
 if __name__ == '__main__':
